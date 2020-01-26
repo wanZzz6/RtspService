a852c50ecc6412702a806dc2c725297c87a18931
wanzheng <1343837706@qq.com>
2020-01-27 01:05:12 +0800

    日志配置

--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
-import uuid
-import cv2
 import logging
+import time
+import uuid
+
+import cv2
 import numpy as np
-
 from PIL import Image
+from logging_config import initLogConf
 
+initLogConf()
 logger = logging.getLogger('RtspService.utils')
 
 
@@ -25,3 +28,20 @@ def array2bytes(ndarray_frame, extension='.jpg'):
 def gen_uuid():
     uuid_4 = uuid.uuid4()
     return uuid_4.hex
+
+
+def calc_run_time(func):
+    """
+    Calculate the running time of the function
+    :param func: the function need to been calculated
+    :return:
+    """
+
+    def call_fun(*args, **kwargs):
+        start_time = time.time()
+        f = func(*args, **kwargs)
+        end_time = time.time()
+        logger.debug('{0}() run time：{1}'.format(func.__name__, (end_time - start_time)))
+        return f
+
+    return call_fun
