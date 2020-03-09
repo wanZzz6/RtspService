f4140845c9a434837f293b067245fb3c9efedd6c
wanzheng <1343837706@qq.com>
2020-03-09 16:12:26 +0800

    sdp 解析类

--- rtsp/sdpp.py
+++ rtsp/sdpp.py
@@ -1,34 +1,38 @@
-import base64
 import json
 import logging
+
+"""
+Pythonic SDP/SDPPLIN Parser
+"""
 
 logger = logging.getLogger('SDP')
 
 
-# Pythonic SDP/SDPPLIN Parser
-
-def _parse_sdpplin_line(item):
-    """ Returns a (name,value) tuple when given an Sdpplin attribute
-    e.g. AvgPacketSize:integer;744 => (AvgPacketSize,744)
-    """
-    name, value = item.split(':', 1)
-    if value.find(';') != -1:
-        # type = value.split(';')[0]
-        # value = value[len(type) + 1:]
-        type, sep, value = value.partition(';')
-        if type == 'integer':
-            value = int(value)
-        if type == 'buffer':
-            value = base64.b64decode(value[1:-1])
-        if type == 'string':
-            value = value[1:-1]
-    return name, value
+# def _parse_sdpplin_line(item):
+#     """ Returns a (name,value) tuple when given an Sdpplin attribute
+#     e.g. AvgPacketSize:integer;744 => (AvgPacketSize,744)
+#     """
+#     name, value = item.split(':', 1)
+#     if value.find(';') != -1:
+#         # type = value.split(';')[0]
+#         # value = value[len(type) + 1:]
+#         type, sep, value = value.partition(';')
+#         if type == 'integer':
+#             value = int(value)
+#         if type == 'buffer':
+#             value = base64.b64decode(value[1:-1])
+#         if type == 'string':
+#             value = value[1:-1]
+#     return name, value
 
 
 class SdpMediaDesc:
     """ SDP Media Desc """
 
     def __init__(self, m):
+        """
+        :param m: start line. m=xxxxxx
+        """
         # 媒体类型描述
         self.type, self.port, self.protocol, self.format = m.strip().split(' ')
         # 只关注 a\b 属性
@@ -75,15 +79,21 @@ class SdpParser(object):
     def __init__(self, data: str = None):
         """ Parses a full SDP data string.
         Alternatively, send lines to the parseLine method. """
-        self.session = None
         # 多个媒体描述
-        self.media = []
+        self.media = None  # type List
+        # 一个会话描述
         self.session = None  # type SdpSessionDesc
         self._last_desc = None
         if data is not None:
             self.parse(data)
 
-    def parse(self, data: str):
+    def parse(self, data: str, verbose=True):
+        """
+        解析 sdp 字符串到 python对象
+        :param data:sdp 字符串
+        :param verbose: 是否打印解析结果
+        :return:
+        """
         # Clear history
         self.media = []
         self.session = SdpSessionDesc()
@@ -95,6 +105,10 @@ class SdpParser(object):
                     self._parseLine(line)
             except Exception as e:
                 logger.error("SDP parse error. %s\n%s" % (line, e))
+
+        if verbose:
+            logger.debug("%s\n%s", self.session, self.media)
+        return self.session, self.media
 
     def _parseLine(self, line):
         """ Parses an SDP line. SDP protocol requires lines be parsed in order
@@ -125,6 +139,7 @@ class SdpParser(object):
         elif type_ == 'b':
             self._last_desc.bandwidth = value
         elif type_ == 'm':
+            # 新的媒体描述开始
             self._last_desc = SdpMediaDesc(value)
             self.media.append(self._last_desc)
 
@@ -136,15 +151,26 @@ class SdpParser(object):
             logger.warning('Unknown SDP Type: %s Value %s', type_, value)
             self._last_desc.other_info[type_] = value
 
+    # todo 构建 sdp 字符串
+    def build_self(self):
+        pass
+
+    def build_new(self, session_desc, media_desc=None):
+        pass
+
     def save_as_json(self, filename):
-        """ Not finished """
+        """ save """
         with open(filename, 'w', encoding='utf-8') as f:
             obj = {'session': self.session.__dict__,
                    'media': [m.__dict__ for m in self.media]}
             f.write(json.dumps(obj))
 
     def loadJson(self, filename):
-        with open(filename, encoding='utf-8') as f:
-            obj = json.loads(f.read())
-            self.session = obj['session']
-            self.media = obj['media']
+        """ load """
+        try:
+            with open(filename, encoding='utf-8') as f:
+                obj = json.loads(f.read())
+                self.session = obj['session']
+                self.media = obj['media']
+        except:
+            logger.error("Load sdp from file fail! - [%s]", filename)
