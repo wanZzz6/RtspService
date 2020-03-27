c208583e12144e9f1acdeac5ae4519e3d821df45
wanzheng <1343837706@qq.com>
2020-03-27 11:54:10 +0800

    跨域

--- RtspService/settings.py
+++ RtspService/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h1fln=2gi3=2!krvkbiklq)#epl57(7%ht1@)_3oh7r(hn_4zk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
